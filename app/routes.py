cfcb7737c2206228619bda64816367b6f0c4870c
JuliannaMali <mali.julianna6@gmail.com>
2024-05-13 16:36:07 +0200

    2024-05-13 JM

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,34 @@
 from app import app
-from flask import  render_template
+from flask import  render_template, request, redirect, url_for
+from bs4 import BeautifulSoup
+import requests
 
 @app.route('/')
 def index():
     return render_template("index.html.jinja")
 
-@app.route('/extract')
+@app.route('/extract', methods=['POST', "GET"])
 def extract():
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+
+        url = f'https://www.ceneo.pl/{product_id}'
+        response = requests.get(url)
+        if response.status_code == requests.codes['ok']:
+            page_dom = BeautifulSoup(response.text, 'html.parse')
+            try:
+                opinions_count = page_dom.select_one('.product-review_link > span').get_text().strip()
+            except AttributeError:
+                opinions_count = 0
+
+            if opinions_count:
+                #proces ekstrakcji
+                return redirect(url_for('product', product_id=product_id))
+        
+            error = "Produkt istnieje, ale  nie ma opinii"
+            return render_template('extract.html.jinja', error=error)
+    
+    error = 'Blędny kod produktu, bądź produkt nie istnieje'
     return render_template("extract.html.jinja")
 
 @app.route('/products')
